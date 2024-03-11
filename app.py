dc624863764f5da3fec55d9653203d51075db219
sachin0034 <161276831+sachin0034@users.noreply.github.com>
2024-03-11 17:52:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import requests
 API_KEY = "sk-j7yf7sf02y12oh0pyiet5k2663t6jbvfcehzmdlv7xu3xo36og8ad4o8weob94ts69"
 
 # Logo image
-logo_image = "Techavtar Favicon.png"
+logo_image = "logo.jpg"
 
 # Hint prompt
 hint_prompt = """
